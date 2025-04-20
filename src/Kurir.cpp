bb5a6f163e15ec28119749ca239043750d286516
adh1ka <hafizhadhi27@gmail.com>
2025-04-21 00:58:52 +0700

    rework

--- src/Kurir.cpp
+++
@@ -1,9 +0,0 @@
-#include "Kurir.h"
-
-Kurir::Kurir(std::string nama, std::string id, std::string kendaraan, int kapasitas)
-    : nama(nama), id(id), kendaraan(kendaraan), kapasitas(kapasitas) {}
-
-std::string Kurir::getID() const { return id; }
-std::string Kurir::getNama() const { return nama; }
-std::string Kurir::getKendaraan() const { return kendaraan; }
-int Kurir::getKapasitas() const { return kapasitas; }
