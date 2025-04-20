02c68b779a738da851914044c9bcb557677a2a98
adh1ka <hafizhadhi27@gmail.com>
2025-04-21 01:01:37 +0700

    part 1 - main and App

---
+++ src/App.cpp
@@ -0,0 +1,7 @@
+#include "App.h"
+#include <iostream>
+
+void App::run() {
+    std::cout << "Selamat datang di Aplikasi Jasa Pengiriman Barang!" << std::endl;
+    // TODO: Tambahkan menu dan logic utama di sini
+}
